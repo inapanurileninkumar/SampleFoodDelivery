b61294f9da21bb8c55afe26b0094cf7e18e3fd76
InapanuriLeninkumar <leninkumar216@gmail.com>
2021-12-09 10:16:01 +0530

    Added support for restoring state on page refresh

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app">
-    <div
-      class="flex-row justify-content-center"
-    >
-      <div
-        class="flex-col-12"
-      >
-        <nav-bar/>
-        <router-view/>
+    <div class="flex-row justify-content-center">
+      <div class="flex-col-12">
+        <nav-bar />
+        <router-view />
       </div>
     </div>
   </div>
@@ -18,21 +14,42 @@ import { restaurantModel } from "./models/restaurantModel";
 import { addressModel } from "./models/addressModel";
 import { mapGetters, mapActions } from "vuex";
 export default {
-  components: { NavBar, },
+  components: { NavBar },
   mixins: [restaurantModel, addressModel],
   computed: {
-    ...(mapGetters({
+    ...mapGetters({
       getRestaurants: "restaurants/getRestaurants",
-    })),
+    }),
+    store: function () {
+      return this.$store;
+    },
   },
   beforeMount: function () {
     this.setupApp();
   },
+  created: function () {
+    // TODO LKI :: REPLACE THIS LOGIC WITH A BETTER SOLUTION TO SAVE STORE STATE ON PAGE REFRESH
+    // Read the state information in sessionStorage when the page loads
+    if (sessionStorage.getItem("store")) {
+      this.$store.replaceState(
+        Object.assign(
+          {},
+          this.$store.state,
+          JSON.parse(sessionStorage.getItem("store"))
+        )
+      );
+    }
+
+    // Save the information in vuex to sessionStorage when the page is refreshed
+    window.addEventListener("beforeunload", () => {
+      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+    });
+  },
   methods: {
-    ...(mapActions({
+    ...mapActions({
       addRestaurant: "restaurants/addRestaurant",
-      addAddress: "user/addAddress"
-    })),
+      addAddress: "user/addAddress",
+    }),
     setupApp: function () {
       if (this.getRestaurants.length) return;
       let numberOfRestaurants = this.getRandomNumber(30, 50);
