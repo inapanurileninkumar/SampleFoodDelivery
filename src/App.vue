e6693eb597c48335a22ac16b1f58c29ec3021242
InapanuriLeninkumar <leninkumar216@gmail.com>
2021-12-09 10:16:29 +0530

    removed dead code in App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,6 @@ export default {
     ...mapGetters({
       getRestaurants: "restaurants/getRestaurants",
     }),
-    store: function () {
-      return this.$store;
-    },
   },
   beforeMount: function () {
     this.setupApp();
